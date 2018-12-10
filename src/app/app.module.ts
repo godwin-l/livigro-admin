8f2fc107e30b2e0e3ae4837bf088743f79054944
Godwin L <alloygodwin1@gmail.com>
2018-12-11 00:28:46 +0530

    datatable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule, MatInputModule,MatFormFieldModule } from '@angular/material';
 import {DataTableModule} from "angular-6-datatable";
-
+import { DataTablesModule } from 'angular-datatables';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -45,6 +45,7 @@ import { MybookingsComponent } from './user/mybookings/mybookings.component';
     MatAutocompleteModule,
     MatFormFieldModule,
     NgxSpinnerModule,
+    DataTablesModule,
     AutoCompleteModule,
     NgbModule.forRoot()
   ],
--- src/app/user/mybookings/mybookings.component.ts
+++ src/app/user/mybookings/mybookings.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import {UserService} from '../user.service';
 import { Router } from '@angular/router';
 import  {ApiService} from '../../api.service';
+import { Http, Response } from '@angular/http';
+import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-mybookings',
@@ -17,11 +20,29 @@ export class MybookingsComponent implements OnInit {
   public userBookings;
   public packid;
   public packageDetail;
+  users$: any[] = [];
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject();
 
-  constructor(private userService:UserService, private apiService:ApiService,private router:Router) { }
+  constructor(private userService:UserService, private apiService:ApiService,private router:Router,private http: HttpClient) { }
 
   ngOnInit() {
-    this.viewbook();
+    this.mobile = this.userService.getMobile();
+    this.userId = this.userService.getUserId();
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 5,
+      processing: true
+    };
+    this.apiService.viewAllbooking().subscribe(data => {
+      this.userBookings = data;
+      console.log(this.userBookings);
+      this.dtTrigger.next();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
   }
 
   public viewbook()
